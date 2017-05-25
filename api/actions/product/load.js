fada93de1f2c4417900ac0fbcf843893e0bf86c3
phut <phut@sendo.vn>
2017-05-25 09:51:40 +0700

    * Xu ly co proxy

--- api/actions/product/load.js
+++ api/actions/product/load.js
@@ -1,14 +1,18 @@
 var rp = require('request-promise');
+var options = {};
+if( process.env.HTTP_PROXY ){
+  options['proxy']  = process.env.HTTP_PROXY;
+}
+var r = rp.defaults(options);
 
 export default function load(req) {
   return new Promise((resolve, reject) => {
     var url = 'https://mapi.sendo.vn/v1/catalog/recomendation?tracking_id=00633019';
-    rp(url).then((html) => {
+    r(url).then((html) => {
       var result = JSON.parse(html);
       resolve(result.result.data);
     }).catch((err)=>{
       reject(err);
     });
   });
-  return ;
 }
