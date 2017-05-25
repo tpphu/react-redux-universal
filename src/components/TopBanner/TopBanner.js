282409abb3945fac1dbf1e85858a538f47db9cf2
phut <phut@sendo.vn>
2017-05-25 13:54:20 +0700

    * Update syntax

--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -4,30 +4,39 @@ import {Carousel } from 'react-bootstrap';
 export default class TopBanner extends Component {
 
   render() {
+    const lists = [
+      {
+        title: 'First slide label',
+        image: 'https://media3.scdn.vn/img2/2016/12_29/T2u6rn_simg_39333c_560x230_maxb.png',
+        description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+      },
+      {
+        title: 'Second slide label',
+        image: 'https://media3.scdn.vn/img2/2017/1_6/SkhTts_simg_39333c_560x230_maxb.png',
+        description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+      },
+      {
+        title: 'Third slide label',
+        image: 'https://media3.scdn.vn/img2/2016/12_29/rFsy8c_simg_39333c_560x230_maxb.jpg',
+        description: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+      }
+    ];
     return (
-      <Carousel>
-        <Carousel.Item>
-          <img width={900} height={500} alt="900x500" src="https://media3.scdn.vn/img2/2016/12_29/T2u6rn_simg_39333c_560x230_maxb.png"/>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width={900} height={500} alt="900x500" src="https://media3.scdn.vn/img2/2017/1_6/SkhTts_simg_39333c_560x230_maxb.png"/>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img width={900} height={500} alt="900x500" src="https://media3.scdn.vn/img2/2016/12_29/rFsy8c_simg_39333c_560x230_maxb.jpg"/>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <div>
+        {lists && lists.length &&
+          <Carousel>
+            {lists.map((item) => (
+              <Carousel.Item>
+                <img width={900} height={500} alt="900x500" src={item.image}/>
+                <Carousel.Caption>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        }
+      </div>
     );
   }
 }
