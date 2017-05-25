c70c571196e75b22ba093083c5babcb8a5cfbc4c
phut <phut@sendo.vn>
2017-05-25 11:53:03 +0700

    * Tao component TopBanner

--- src/components/index.js
+++ src/components/index.js
@@ -12,3 +12,4 @@ export MiniInfoBar from './MiniInfoBar/MiniInfoBar';
 export SurveyForm from './SurveyForm/SurveyForm';
 export WidgetForm from './WidgetForm/WidgetForm';
 export ProductForm from './ProductForm/ProductForm';
+export TopBanner from './TopBanner/TopBanner';
--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -5,7 +5,8 @@ import * as productActions from 'redux/modules/products';
 import {isLoaded, load as loadProducts} from 'redux/modules/products';
 import {initializeWithKey} from 'redux-form';
 import { asyncConnect } from 'redux-async-connect';
-import { Grid, Row, Col, Carousel } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
+import {TopBanner} from 'components';
 
 @asyncConnect([{
   deferred: true,
@@ -48,29 +49,7 @@ export default class Products extends Component {
           </button>
         </h1>
         <Helmet title="products"/>
-        <Carousel>
-          <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src="https://media3.scdn.vn/img2/2016/12_29/T2u6rn_simg_39333c_560x230_maxb.png"/>
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src="https://media3.scdn.vn/img2/2017/1_6/SkhTts_simg_39333c_560x230_maxb.png"/>
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img width={900} height={500} alt="900x500" src="https://media3.scdn.vn/img2/2016/12_29/rFsy8c_simg_39333c_560x230_maxb.jpg"/>
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+        <TopBanner />
         {products && products.length &&
         <Grid>
           <Row className="show-grid">
